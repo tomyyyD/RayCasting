e687ea7ad4fc509a87ee2ab3fd1ec8e1f9d441ea
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2022-07-31 18:07:24 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    float ray[] = {1.0f, 0.0f, 0.0f};
+
+    float vertices[] = {
+            -0.5, 0.5, //top left
+            -0.5, -0.5, //bottom left
+            0.5, 0.5, //top right
+            0.5, -0.5 //bottom right
+    };
+
+
+
+
+}
